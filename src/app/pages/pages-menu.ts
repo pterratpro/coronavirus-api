5126d869ace0ef34907532827c616ec47572875c
pterratpro <pierreterrat.pro@gmail.com>
2020-07-03 14:47:03 +0200

    init(src): source files initializaiton

---
+++ src/app/pages/pages-menu.ts
@@ -0,0 +1,15 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'E-commerce',
+    icon: 'home',
+    link: '/pages/dashboard',
+    home: true,
+  },
+  {
+    title: 'IoT Dashboard',
+    icon: 'shopping-cart',
+    link: '/pages/iot-dashboard',
+  },
+];
