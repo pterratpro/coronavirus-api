7690289112e2c835b55c7cd606faea34e2135c71
pterratpro <pierreterrat.pro@gmail.com>
2020-07-03 14:58:11 +0200

    init(header): clean header

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -2,14 +2,9 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'E-commerce',
+    title: 'Home',
     icon: 'home',
-    link: '/pages/dashboard',
+    link: '/',
     home: true,
-  },
-  {
-    title: 'IoT Dashboard',
-    icon: 'shopping-cart',
-    link: '/pages/iot-dashboard',
   },
 ];
